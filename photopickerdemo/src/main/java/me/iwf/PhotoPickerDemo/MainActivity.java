60d1cf54a97ed542b52d2c360e54020dceeee229
ilia.berlana <ilia.berlana@wefoxgroup.com>
2019-10-28 11:24:29 +0100

    New request to permissions

--- photopickerdemo/src/main/java/me/iwf/PhotoPickerDemo/MainActivity.java
+++ photopickerdemo/src/main/java/me/iwf/PhotoPickerDemo/MainActivity.java
@@ -1,107 +1,131 @@
 package me.iwf.PhotoPickerDemo;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.OrientationHelper;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.View;
+
 import java.util.ArrayList;
 import java.util.List;
+
 import me.iwf.photopicker.PhotoPickerActivity;
 import me.iwf.photopicker.utils.PhotoPickerIntent;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+    public final static int MY_PERMISSIONS_REQUEST = 12;
+
+    RecyclerView recyclerView;
+    PhotoAdapter photoAdapter;
+
+    ArrayList<String> selectedPhotos = new ArrayList<>();
+
+    public final static int REQUEST_CODE = 1;
 
 
-  RecyclerView recyclerView;
-  PhotoAdapter photoAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-  ArrayList<String> selectedPhotos = new ArrayList<>();
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        photoAdapter = new PhotoAdapter(this, selectedPhotos);
 
-  public final static int REQUEST_CODE = 1;
-
-
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-
-    recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-    photoAdapter = new PhotoAdapter(this, selectedPhotos);
-
-    recyclerView.setLayoutManager(new StaggeredGridLayoutManager(4, OrientationHelper.VERTICAL));
-    recyclerView.setAdapter(photoAdapter);
+        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(4, OrientationHelper.VERTICAL));
+        recyclerView.setAdapter(photoAdapter);
 
 
-    findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View view) {
-        PhotoPickerIntent intent = new PhotoPickerIntent(MainActivity.this);
-        ArrayList<String> selected_photos = new ArrayList<String>();
-        selected_photos.add("/storage/emulated/0/Pictures/JPEG_20150826_131652_-1972212360.jpg");
-        selected_photos.add("/storage/emulated/0/Pictures/JPEG_20150908_124042_280811053.jpg");
-        intent.setSelectedPhotos(selected_photos);
-        intent.setActivityTitle("JOTO");
-        intent.setPhotoCount(9);
-        startActivityForResult(intent, REQUEST_CODE);
-      }
-    });
+        pickPermission();
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PhotoPickerIntent intent = new PhotoPickerIntent(MainActivity.this);
+                ArrayList<String> selected_photos = new ArrayList<String>();
+                selected_photos.add("/storage/emulated/0/Pictures/JPEG_20150826_131652_-1972212360.jpg");
+                selected_photos.add("/storage/emulated/0/Pictures/JPEG_20150908_124042_280811053.jpg");
+                intent.setSelectedPhotos(selected_photos);
+                intent.setActivityTitle("JOTO");
+                intent.setPhotoCount(9);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
 
 
-    findViewById(R.id.button_no_camera).setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        PhotoPickerIntent intent = new PhotoPickerIntent(MainActivity.this);
-        intent.setPhotoCount(7);
-        intent.setShowCamera(false);
-        startActivityForResult(intent, REQUEST_CODE);
-      }
-    });
+        findViewById(R.id.button_no_camera).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PhotoPickerIntent intent = new PhotoPickerIntent(MainActivity.this);
+                intent.setPhotoCount(7);
+                intent.setShowCamera(false);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
 
 
-    findViewById(R.id.button_one_photo).setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        PhotoPickerIntent intent = new PhotoPickerIntent(MainActivity.this);
-        intent.setPhotoCount(1);
-        intent.setShowCamera(true);
-        startActivityForResult(intent, REQUEST_CODE);
-      }
-    });
+        findViewById(R.id.button_one_photo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PhotoPickerIntent intent = new PhotoPickerIntent(MainActivity.this);
+                intent.setPhotoCount(1);
+                intent.setShowCamera(true);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
 
-    findViewById(R.id.button_photo_gif).setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        PhotoPickerIntent intent = new PhotoPickerIntent(MainActivity.this);
-        intent.setPhotoCount(4);
-        intent.setShowCamera(true);
-        intent.setShowGif(true);
-        startActivityForResult(intent, REQUEST_CODE);
-      }
-    });
+        findViewById(R.id.button_photo_gif).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PhotoPickerIntent intent = new PhotoPickerIntent(MainActivity.this);
+                intent.setPhotoCount(4);
+                intent.setShowCamera(true);
+                intent.setShowGif(true);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
 
-  }
-
-
-  public void previewPhoto(Intent intent) {
-    startActivityForResult(intent, REQUEST_CODE);
-  }
-
-
-  @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    super.onActivityResult(requestCode, resultCode, data);
-
-    List<String> photos = null;
-    if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
-      if (data != null) {
-        photos = data.getStringArrayListExtra(PhotoPickerActivity.KEY_SELECTED_PHOTOS);
-      }
-      selectedPhotos.clear();
-
-      if (photos != null) {
-
-        selectedPhotos.addAll(photos);
-      }
-      photoAdapter.notifyDataSetChanged();
     }
-  }
+
+    private void pickPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                    || checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+                    || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST);
+            }
+        }
+    }
+
+
+    public void previewPhoto(Intent intent) {
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        List<String> photos = null;
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+            if (data != null) {
+                photos = data.getStringArrayListExtra(PhotoPickerActivity.KEY_SELECTED_PHOTOS);
+            }
+            selectedPhotos.clear();
+
+            if (photos != null) {
+
+                selectedPhotos.addAll(photos);
+            }
+            photoAdapter.notifyDataSetChanged();
+        }
+    }
 
 
 }
